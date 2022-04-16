d9bbd17c7c625974dcd0c31f78f143f8684d8bd8
abdelhaqelamraoui <abdelhaqelamraoui@gmail.com>
2022-04-16 22:51:35 +0000

    playing control + new UI

--- app/src/main/java/com/example/ansariyat/MainActivity.java
+++ app/src/main/java/com/example/ansariyat/MainActivity.java
@@ -7,6 +7,7 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.ListView;
 import android.widget.Toast;
 
@@ -15,8 +16,12 @@ import java.util.HashMap;
 public class MainActivity extends AppCompatActivity {
 
     ListView listView;
+    Button buttonPrevious;
+    Button buttonPause;
+    Button buttonNext;
     MediaPlayer mediaPlayer;
     HashMap<String,Integer> audioTracks;
+    Integer currentAudioId = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,6 +29,9 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         listView = (ListView) findViewById(R.id.listView);
+        buttonPrevious = (Button) findViewById(R.id.button_previous);
+        buttonPause = (Button) findViewById(R.id.button_pause);
+        buttonNext = (Button) findViewById(R.id.button_next);
 
         audioTracks = new HashMap<String, Integer>();
         audioTracks.put("أسرار التعبد بالقرآن", R.raw.asrar_taabbod_blikoran);
@@ -33,19 +41,72 @@ public class MainActivity extends AppCompatActivity {
 
         String[] titles = audioTracks.keySet().toArray(new String[0]);
 
-        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, titles);
-        listView.setAdapter(adapter);
+        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(
+                //context
+                this,
+                //layout
+                R.layout.audios_rows,
+                //row (view) which which holds the data to be shown
+                R.id.row_text,
+                //data (an Array of String)
+                titles);
+
+        listView.setAdapter(arrayAdapter);
         listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
-                if(mediaPlayer != null) {
-                    mediaPlayer.release();
-                }
-                int id = audioTracks.get(titles[i]);
-                mediaPlayer = MediaPlayer.create(getApplicationContext(), id);
-                mediaPlayer.seekTo(0);
-                mediaPlayer.start();
+                currentAudioId = i;
+                playAudioById(i);
             }
         });
+
+        buttonPrevious.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(mediaPlayer == null || ! mediaPlayer.isPlaying())
+                    return;
+                playAudioById(--currentAudioId);
+            }
+        });
+
+        buttonPause.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(mediaPlayer == null)
+                    return;
+                if(mediaPlayer.isPlaying()) {
+                    mediaPlayer.pause();
+                    buttonPause.setText("Play");
+                } else {
+                    mediaPlayer.start();
+                    buttonPause.setText("Pause");
+                }
+            }
+        });
+
+        buttonNext.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(mediaPlayer == null || ! mediaPlayer.isPlaying())
+                    return;
+                playAudioById(++currentAudioId);
+            }
+        });
+
+    }
+
+    private void  playAudioById(int i) {
+        if(i < 0 || i > audioTracks.size() - 1)
+            return;
+        if(mediaPlayer != null)
+            mediaPlayer.release();
+
+        String[] titles = audioTracks.keySet().toArray(new String[0]);
+        int id = audioTracks.get(titles[i]);
+        mediaPlayer = MediaPlayer.create(getApplicationContext(), id);
+        mediaPlayer.seekTo(0);
+        mediaPlayer.start();
+        buttonPause.setText("Pause");
+
     }
 }
