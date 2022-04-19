47480903479c504f6494128c9810aff6864b5137
abdelhaqelamraoui <abdelhaqelamraoui@gmail.com>
2022-04-19 15:37:06 +0000

    adding optionMenu + compressed audio files + maghribi font + pic logo

--- app/src/main/java/com/example/ansariyat/MainActivity.java
+++ app/src/main/java/com/example/ansariyat/MainActivity.java
@@ -1,24 +1,35 @@
 package com.example.ansariyat;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.app.Dialog;
+import android.content.pm.ActivityInfo;
 import android.media.MediaPlayer;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
-import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.LinearLayout;
 import android.widget.ListView;
 import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.material.snackbar.Snackbar;
 
 import java.util.HashMap;
 
 public class MainActivity extends AppCompatActivity {
 
     ListView listView;
-    Button buttonPrevious;
-    Button buttonPause;
-    Button buttonNext;
+    ImageButton buttonPrevious;
+    ImageButton buttonPause;
+    ImageButton buttonNext;
+    LinearLayout linearLayout;
+    LinearLayout lastSelectedItem;
     MediaPlayer mediaPlayer;
     HashMap<String,Integer> audioTracks;
     Integer currentAudioId = 0;
@@ -27,13 +38,18 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LOCKED); // set screen orientation to portrait
         listView = (ListView) findViewById(R.id.listView);
-        buttonPrevious = (Button) findViewById(R.id.button_previous);
-        buttonPause = (Button) findViewById(R.id.button_pause);
-        buttonNext = (Button) findViewById(R.id.button_next);
+        buttonPrevious = findViewById(R.id.button_previous);
+        buttonPause = (ImageButton) findViewById(R.id.button_pause);
+        buttonNext = (ImageButton) findViewById(R.id.button_next);
+        linearLayout = (LinearLayout) findViewById(R.id.linearLayout);
+        buttonPrevious.setImageResource(android.R.drawable.ic_media_previous);
+        buttonPause.setImageResource(android.R.drawable.ic_media_play);
+        buttonNext.setImageResource(android.R.drawable.ic_media_next);
 
-        audioTracks = new HashMap<String, Integer>();
+
+        audioTracks = new HashMap<>();
         audioTracks.put("أسرار التعبد بالقرآن", R.raw.asrar_taabbod_blikoran);
         audioTracks.put("صفات العبد الرباني", R.raw.sifat_alaabd_arabbani);
         audioTracks.put("سنة الاستدراج", R.raw.sonnato_al_istidraj);
@@ -63,7 +79,7 @@ public class MainActivity extends AppCompatActivity {
         buttonPrevious.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                if(mediaPlayer == null || ! mediaPlayer.isPlaying())
+                if(mediaPlayer == null || ! mediaPlayer.isPlaying() || currentAudioId == 0)
                     return;
                 playAudioById(--currentAudioId);
             }
@@ -72,14 +88,16 @@ public class MainActivity extends AppCompatActivity {
         buttonPause.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                if(mediaPlayer == null)
+                if(mediaPlayer == null) {
+                    playAudioById(0);
                     return;
+                }
                 if(mediaPlayer.isPlaying()) {
                     mediaPlayer.pause();
-                    buttonPause.setText("Play");
+                    buttonPause.setImageResource(android.R.drawable.ic_media_play);
                 } else {
                     mediaPlayer.start();
-                    buttonPause.setText("Pause");
+                    buttonPause.setImageResource(android.R.drawable.ic_media_pause);
                 }
             }
         });
@@ -87,7 +105,7 @@ public class MainActivity extends AppCompatActivity {
         buttonNext.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                if(mediaPlayer == null || ! mediaPlayer.isPlaying())
+                if(mediaPlayer == null || ! mediaPlayer.isPlaying() || currentAudioId == audioTracks.size() - 1)
                     return;
                 playAudioById(++currentAudioId);
             }
@@ -95,18 +113,50 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.option_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        int id = item.getItemId();
+        switch (id) {
+            case R.id.suggest:
+                Snackbar.make(linearLayout, "Suggest", Snackbar.LENGTH_LONG).show();
+
+                return true;
+            case R.id.about:
+                Snackbar.make(linearLayout, "About", Snackbar.LENGTH_SHORT).show();
+                return true;
+            case R.id.rateUs:
+                Snackbar.make(linearLayout, "RateUs", Snackbar.LENGTH_SHORT).show();
+                return true;
+        }
+        return true;
+    }
+
+
+
     private void  playAudioById(int i) {
         if(i < 0 || i > audioTracks.size() - 1)
             return;
         if(mediaPlayer != null)
             mediaPlayer.release();
 
+        if(lastSelectedItem != null)
+            lastSelectedItem.setBackgroundColor(getResources().getColor(R.color.listItem));
+        LinearLayout ll = (LinearLayout) listView.getChildAt(i);
+        ll.setBackgroundColor(getResources().getColor(R.color.selecteditem));
+        lastSelectedItem = ll;
+
         String[] titles = audioTracks.keySet().toArray(new String[0]);
         int id = audioTracks.get(titles[i]);
         mediaPlayer = MediaPlayer.create(getApplicationContext(), id);
         mediaPlayer.seekTo(0);
         mediaPlayer.start();
-        buttonPause.setText("Pause");
+        buttonPause.setImageResource(android.R.drawable.ic_media_pause);
 
     }
 }
